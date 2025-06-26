9974013285f1bd4d2307ff8ec3f1ab2df3d7b2c4
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-06-26 10:52:12 -0500

    Added methods to the album and movie controller

--- src/main/java/com/example/springBoot2/controllers/AlbumController.java
+++ src/main/java/com/example/springBoot2/controllers/AlbumController.java
@@ -1,21 +1,47 @@
 package com.example.springBoot2.controllers;
 
 import com.example.springBoot2.models.Album;
+import com.example.springBoot2.repositories.AlbumRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/albums")
 public class AlbumController {
-    private final List<Album> albums = List.of(
-        new Album("The Dark Side of the Moon", "Pink Floyd", 1973, 10),
-        new Album("Back in Black", "AC/DC", 1980, 10),
-        new Album("The Bodyguard", "Whitney Houston", 1992, 10)
-    );
+    private final AlbumRepository albumRepository;
 
-    @GetMapping
-    public List<Album> getAlbums() {
-        return albums;
+    public AlbumController(AlbumRepository albumRepository){
+        this.albumRepository = albumRepository;
     }
+    //Implement the getAllItems method in each of the controllers. This method should call the findAll method on the JPA repository and return the result, and should be annotated with @GetMapping//
+    @GetMapping("")
+    public List<Album> getAllItems (){
+        return albumRepository.findAll();
+    }
+
+    //Implement the getItem method in each of the controllers. This method should call the findById method on the JPA repository and return the result, and should be annotated with @GetMapping('/{id}') and have a mapped @PathVariable parameter for the id of type int.//
+    @GetMapping("/albums/{id}")
+    public Optional<Album> getItem(@PathVariable int id){
+        return albumRepository.findById(id);
+    }
+    //Implement the addItem method in each of the controllers. This method should call the save method on the JPA repository and return the result, and should be annotated with @PostMapping with a @RequestBody parameter//
+    @PostMapping("/albums")
+    public Album addItem (@RequestBody Album album){
+        return albumRepository.save(album);
+    }
+    //Implement the updateItem method in each of the controllers. This method should call the save method on the JPA repository and return the result, and should be annotated with @PutMapping('/{id}') with a @PathVariable parameter for the id of type int and a @RequestBody parameter.//
+    @PutMapping ("/albums/{id}")
+    public Album updateItem (@PathVariable int id, @RequestBody Album album){
+        album.setId(id);
+        return albumRepository.save(album);
+    }
+    //Implement the deleteItem method in each of the controllers. This method should call the deleteById method on the JPA repository and should be annotated with @DeleteMapping('/{id}') with a @PathVariable parameter for the id of type int.
+    @DeleteMapping("/albums/{id}")
+    public void deleteItem(@PathVariable int id){
+        albumRepository.deleteById(id);
+    }
+
+
 }
--- src/main/java/com/example/springBoot2/controllers/MovieController.java
+++ src/main/java/com/example/springBoot2/controllers/MovieController.java
@@ -1,21 +1,46 @@
 package com.example.springBoot2.controllers;
 
 import com.example.springBoot2.models.Movie;
+import com.example.springBoot2.repositories.MovieRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
-    private final List<Movie> movies = List.of(
-        new Movie("The Shawshank Redemption", "Frank Darabont", 1994, 142),
-        new Movie("The Godfather", "Francis Ford Coppola", 1972, 175),
-        new Movie("The Dark Knight", "Christopher Nolan", 2008, 152)
-    );
+    private final MovieRepository movieRepository;
 
-    @GetMapping
-    public List<Movie> getMovies() {
-        return movies;
+    public MovieController(MovieRepository movieRepository){
+        this.movieRepository = movieRepository;
+    }
+
+    //Implement the getAllItems method in each of the controllers. This method should call the findAll method on the JPA repository and return the result, and should be annotated with @GetMapping//
+    @GetMapping("")
+    public List<Movie> getAllItems (){
+        return movieRepository.findAll();
+    }
+
+    //Implement the getItem method in each of the controllers. This method should call the findById method on the JPA repository and return the result, and should be annotated with @GetMapping('/{id}') and have a mapped @PathVariable parameter for the id of type int.//
+    @GetMapping("/movies/{id}")
+    public Optional<Movie> getItem(@PathVariable int id){
+        return movieRepository.findById(id);
+    }
+    //Implement the addItem method in each of the controllers. This method should call the save method on the JPA repository and return the result, and should be annotated with @PostMapping with a @RequestBody parameter//
+    @PostMapping("/movies")
+    public Movie addItem (@RequestBody Movie movie){
+        return movieRepository.save(movie);
+    }
+    //Implement the updateItem method in each of the controllers. This method should call the save method on the JPA repository and return the result, and should be annotated with @PutMapping('/{id}') with a @PathVariable parameter for the id of type int and a @RequestBody parameter.//
+    @PutMapping ("/movies/{id}")
+    public Movie updateItem (@PathVariable int id, @RequestBody Movie movie){
+        movie.setId(id);
+        return movieRepository.save(movie);
+    }
+    //Implement the deleteItem method in each of the controllers. This method should call the deleteById method on the JPA repository and should be annotated with @DeleteMapping('/{id}') with a @PathVariable parameter for the id of type int.
+    @DeleteMapping("/movies/{id}")
+    public void deleteItem(@PathVariable int id){
+        movieRepository.deleteById(id);
     }
 }
